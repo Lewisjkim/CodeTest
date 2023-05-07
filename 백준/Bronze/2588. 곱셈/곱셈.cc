071328bae23cbd4f28a752ddcddc700726db28cd
JuSeong Kim (Lewis Kim) <93592201+Lewisjkim@users.noreply.github.com>
2023-05-08 05:37:04 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int a;
+    string b;
+    cin >> a;
+    cin >> b;
+
+
+    cout << a * (b[2] - '0') << endl;
+    cout << a * (b[1] - '0') << endl;
+    cout << a * (b[0] - '0') << endl;
+    cout << a * stoi(b); 
+    return 0;
+}
