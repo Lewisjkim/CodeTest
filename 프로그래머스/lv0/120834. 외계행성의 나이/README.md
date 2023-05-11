571716d87904b5862356fa9a3ac431e6814e316b
JuSeong Kim (Lewis Kim) <93592201+Lewisjkim@users.noreply.github.com>
2023-05-12 01:23:04 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/README.md
+++ 프로그래머스/lv0/120834. 외계행성의 나이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 외계행성의 나이 - 120834 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120834) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120834?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.02 ms
+메모리: 4.2 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.cpp
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.cpp
@@ -8,26 +8,7 @@ string solution(int age) {
     string s_age = to_string(age);
     for(auto& a : s_age)
     {
-        if(a == '0')
-            answer += 'a';
-        else if(a == '1')
-            answer += 'b';
-        else if(a == '2')
-            answer += 'c';
-        else if(a == '3')
-            answer += 'd';
-        else if(a == '4')
-            answer += 'e';
-        else if(a == '5')
-            answer += 'f';
-        else if(a == '6')
-            answer += 'g';
-        else if(a == '7')
-            answer += 'h';
-        else if(a == '8')
-            answer += 'i';
-        else if(a == '9')
-            answer += 'j';
+        a += 49;
     }
-    return answer;
+    return answer = s_age;
 }
